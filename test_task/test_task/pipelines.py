74ac53b7e34e8a9bf8828b3588c4c73e3d58712a
Andrej <andrejdzenisevich@alidainc.com>
2023-01-19 16:35:05 +0300

    3 commit

---
+++ test_task/test_task/pipelines.py
@@ -0,0 +1,15 @@
+import csv
+
+
+class ProductPipeline:
+
+    def open_spider(self, spider):
+        self.file = open('test_task/data/test.csv', 'w', newline='')
+        self.writer = csv.writer(self.file)
+        self.writer.writerow(['Name', 'Price', 'Image'])
+
+    def close_spider(self, spider):
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.writer.writerow(item.values())
