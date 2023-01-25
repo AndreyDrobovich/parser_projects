b5dfdd5ac2a7eaa0be2e2c2f9c8b1645e5532767
Andrej <andrejdzenisevich@alidainc.com>
2023-01-25 15:26:01 +0300

    Add realt  parser

---
+++ dags/services/scrapy_code/configs/spider_mapping.py
@@ -0,0 +1,7 @@
+DICT_MAPPING = {
+    "Общая": "main_area",
+    "Жилая": "living_area",
+    "Этаж": "floor",
+    "Построен": "year",
+    "Кухня": "kitchen_area",
+}
--- dags/services/scrapy_code/settings.py
+++ dags/services/scrapy_code/settings.py
@@ -3,6 +3,7 @@ SPIDER_MODULES = ["dags.services.scrapy_code.spiders"]
 NEWSPIDER_MODULE = "dags.services.scrapy_code.spiders"
 ROBOTSTXT_OBEY = False
 ITEM_PIPELINES = {
-    "dags.services.scrapy_code.pipelines.ProductPipeline": 300,
+    "dags.services.scrapy_code.pipelines.ProductPipeline": 800,
+    "dags.services.scrapy_code.pipelines.JsonWriterPipeline": 300,
 }
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
